dfc4dd9bbed7a08713f72086581723ae67a18b5b
andreLzac <andrepicoal@gmail.com>
2026-04-24 04:36:09 -0300

    Update teste.py
    Calculadora de IMC
    primeiro projeto "usavel" ;]

--- teste.py
+++ teste.py
@@ -1,2 +1,23 @@
-#teste para projeto 
-print("ola mundo - andre é um dev bacana")
+nome = input('Digite seu nome')
+print (f'Ola {nome}')
+peso = float(input('Agora digite seu peso').replace(",", "."))
+print (f'Otimo!')
+altura = float(input('Digite sua altura').replace(",", "."))
+
+imc = peso / (altura ** 2)
+
+# Classificações
+if imc < 18.5:
+    classificacao = "abaixo do peso"
+elif imc < 25:
+    classificacao = "peso normal"
+elif imc < 30:
+    classificacao = "sobre peso"
+elif imc < 35:
+    classificacao = "obesidade grau 1"
+elif imc < 40:
+    classificacao = "obesidade grau 2"
+else:
+    classificacao = "obesidade grau 3"
+
+print(f'sr. {nome} seu imc é {imc:.2f} em base do seu imc você esta na categoria {classificacao}, obrigado!')
